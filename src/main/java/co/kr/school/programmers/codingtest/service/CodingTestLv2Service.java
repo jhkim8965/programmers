29cdb7cb16a8af00f8010e2d51d0adcb95fb0119
jhkim <jinhyun3253@gmail.com>
2023-01-20 14:20:11 +0900

    프로그래머스 > Java > Lv2 > 다리를 지나는 트럭 문제풀이

--- src/main/java/co/kr/school/programmers/codingtest/service/CodingTestLv2Service.java
+++ src/main/java/co/kr/school/programmers/codingtest/service/CodingTestLv2Service.java
@@ -1,11 +1,13 @@
 package co.kr.school.programmers.codingtest.service;
 
+import co.kr.school.programmers.codingtest.lessons.lv2.Lesson_42583;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CodingTestLv2Service {
 
     /**
-     * 코딩테스트 lv1: 성격 유형 검사하기
+     * 코딩테스트 Lv2: 다리를 지나는 트럭
      */
+    public int lesson_42583(int bridge_length, int weight, int[] truck_weights) { return new Lesson_42583(bridge_length, weight, truck_weights).getAnswer(); }
 }
--- src/test/java/co/kr/school/programmers/codingtest/controller/CodingTestLv2ControllerTest.java
+++ src/test/java/co/kr/school/programmers/codingtest/controller/CodingTestLv2ControllerTest.java
@@ -1,10 +1,15 @@
 package co.kr.school.programmers.codingtest.controller;
 
 import co.kr.school.programmers.codingtest.service.CodingTestLv2Service;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,4 +19,24 @@ class CodingTestLv2ControllerTest {
 
     @Autowired
     CodingTestLv2Service codingTestLv2Service;
+
+    @Test
+    @DisplayName("다리를 지나는 트럭")
+    void lesson_42583(){
+        // given
+//        int bridge_length = 2;
+        int bridge_length = 100;
+//        int weight = 10;
+        int weight = 100;
+//        int[] truck_weights = {7, 4, 5, 6};
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+
+        // when
+        int answer = codingTestLv2Service.lesson_42583(bridge_length, weight, truck_weights);
+        System.out.println("answer = " + answer);
+
+        // then
+//        assertThat(answer).isEqualTo(8);
+        assertThat(answer).isEqualTo(110);
+    }
 }
